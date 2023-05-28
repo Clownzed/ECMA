ca4ef9b82ebd495a6980f60479560836086d873b
cnorzen <binhdtph15131@fpt.edu.vn>
2023-05-29 05:10:00 +0700

    init project

---
+++ ECM/main.js
@@ -0,0 +1,8 @@
+// khai báo biến và chứa thư viện typed
+var options = {
+    strings: ['Binh','Cnorzen', 'Design','Front-end'],
+    typeSpeed: 100, // thuộc tính xác định tốc độ gõ chữ 100ms
+    backSpeed:100, // thuộc tính xác định xóa chữ 100ms
+    loop: true // lặp lại
+  };
+var typed = new Typed('.typing', options);
